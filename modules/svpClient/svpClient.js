418daea25acbc3603da58fdd8bfa7c5f170a5b02
Ahmed Azraq <aazraq@eg.ibm.com>
2016-12-14 23:45:31 -0600

    Calculate ETB without weather

---
+++ modules/svpClient/svpClient.js
@@ -0,0 +1,26 @@
+/*eslint-env node */
+//Request Module
+var httpRequest = require('request');
+
+
+//queries SVP for Event
+exports.queryEvent = function(objectId, svpQueryEventEndpoint, clientId, callback) {
+	var options = {
+		url: svpQueryEventEndpoint+'?objectId=' + objectId,
+		headers: {
+			'x-ibm-client-id': clientId
+		}
+	};
+	httpRequest(
+		options,
+		function(error, response, body) {
+			try {
+				var json = JSON.parse(body);
+				var event = json.Envelope.Body.queryResponse.events.event;
+				callback(null, event);
+			} catch (e) {
+				callback(e, null);
+			}
+		}
+	);
+};
