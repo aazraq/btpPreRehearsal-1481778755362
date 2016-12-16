1542d93c063065cef794b00c85dc8319d1c0336b
Ahmed Azraq <aazraq@eg.ibm.com>
2016-12-16 02:14:24 -0600

    get the latest event

--- modules/svpClient/svpClient.js
+++ modules/svpClient/svpClient.js
@@ -6,7 +6,7 @@ var httpRequest = require('request');
 //queries SVP for Event
 exports.queryEvent = function(objectId, svpQueryEventEndpoint, clientId, callback) {
 	var options = {
-		url: svpQueryEventEndpoint+'?objectId=' + objectId,
+		url: svpQueryEventEndpoint + '?objectId=' + objectId,
 		headers: {
 			'x-ibm-client-id': clientId
 		}
@@ -16,8 +16,11 @@ exports.queryEvent = function(objectId, svpQueryEventEndpoint, clientId, callbac
 		function(error, response, body) {
 			try {
 				var json = JSON.parse(body);
-				var event = json.Envelope.Body.queryResponse.events.event;
-				callback(null, event);
+				var events = json.Envelope.Body.queryResponse.events.event;
+				if (events.length === null || events.length > 0) {
+					events = events[events.length - 1];
+				}
+				callback(null, events);
 			} catch (e) {
 				callback(e, null);
 			}
